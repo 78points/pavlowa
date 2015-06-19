1888f09f23a6fb87b4b14aaebc258c64a5def29a
Pawel <78points@gmail.com>
2015-06-19 14:24:40 +0100

    Refactoring

--- view/contact.php
+++ view/contact.php
@@ -102,69 +102,69 @@ include('included/header.php');
 
 <div id="map-canvas" class="map-canvas col-sm-6"></div>
 
-   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
-    <script type="text/javascript">
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+<script type="text/javascript">
 
 
-var pos = new google.maps.LatLng( 53.2205654, -6.6593078);
-var marker;
-var map;        
+  var pos = new google.maps.LatLng( 53.2205654, -6.6593078);
+  var marker;
+  var map;        
 
-        function initialize() {
-            "use strict";
-            var roadAtlasStyles = [
+  function initialize() {
+      "use strict";
+      var roadAtlasStyles = [
 
-              {
-                "stylers": [
-                  { "saturation": -81 },
-                  { "hue": "#00e5ff" }
-                ]
-              },{
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                  { "visibility": "off" }
-                ]
-              }
-
-            ];
-
-
-            var mapOptions = {
-                zoom: 15,
-                center: pos,           
-                mapTypeControlOptions: {
-                    mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
-                }
-            };
-
-            map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);
-
-            
-     var image = 'img/map-icon.png';
-  marker = new google.maps.Marker({
-    map:map,
-  icon: image,
-    draggable:true,
-    animation: google.maps.Animation.DROP,
-    position: pos
-  });       
-            
-            var styledMapOptions = {
-                name: "Styled Map",
-            };
-
-            var usRoadMapType = new google.maps.StyledMapType(
-                roadAtlasStyles, styledMapOptions );
-
-            map.mapTypes.set('usroadatlas', usRoadMapType);
-            map.setMapTypeId('usroadatlas');
+        {
+          "stylers": [
+            { "saturation": -81 },
+            { "hue": "#00e5ff" }
+          ]
+        },{
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            { "visibility": "off" }
+          ]
         }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
-        
-        
-    </script>
+      ];
+
+
+      var mapOptions = {
+          zoom: 15,
+          center: pos,           
+          mapTypeControlOptions: {
+              mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
+          }
+      };
+
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+
+      
+       var image = 'img/map-icon.png';
+      marker = new google.maps.Marker({
+      map:map,
+      icon: image,
+      draggable:true,
+      animation: google.maps.Animation.DROP,
+      position: pos
+      });       
+      
+      var styledMapOptions = {
+          name: "Styled Map",
+      };
+
+      var usRoadMapType = new google.maps.StyledMapType(
+          roadAtlasStyles, styledMapOptions );
+
+      map.mapTypes.set('usroadatlas', usRoadMapType);
+      map.setMapTypeId('usroadatlas');
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+    
+    
+</script>
 
 
 
@@ -185,31 +185,28 @@ var map;
 <script type='text/javascript'>
 
 $(document).ready( function() {
-    $("#form").submit(function () {
-            $.post("included/subscribe.php",$("#form").serialize(),function(texte){
-var texte = jQuery.parseJSON(texte);
-$('#alert').show();
-if(texte=="saved"){
+  $("#form").submit(function () {
+    $.post("included/subscribe.php",$("#form").serialize(),function(texte){
+    var texte = jQuery.parseJSON(texte);
+    $('#alert').show();
+    if(texte=="saved"){
+      document.getElementById('zoneSub').innerHTML='<div class="alert alert-success" role="alert">Thank you for your message.</div>';
+      $('#form').html('<div class="clear" style="height:100px;"></div>');
+    }
+    else if (texte=="email_error"){
+     document.getElementById('zoneSub').innerHTML='<div class="alert alert-danger" role="alert">Sorry. Message cannot be sent at this time.</div>';
+    } 
+    else if(texte=="invalid"){
+     document.getElementById('zoneSub').innerHTML='<div class="alert alert-danger" role="alert">This email is invalid</div>';
+    }
 
-document.getElementById('zoneSub').innerHTML='<div class="alert alert-success" role="alert">Thank you for your message.</div>';
-
-$('#form').html('<div class="clear" style="height:100px;"></div>');
-
-}
-else if (texte=="email_error"){
-document.getElementById('zoneSub').innerHTML='<div class="alert alert-danger" role="alert">Sorry. Message cannot be sent at this time.</div>';
-} 
-else if(texte=="invalid"){
-document.getElementById('zoneSub').innerHTML='<div class="alert alert-danger" role="alert">This email is invalid</div>';
-}
-
-});
-return false; // ne change pas de page
-});
+    });
+     return false; // ne change pas de page
+  });
 });
 
-$(document).ready(function() {
-$('#alert').hide();
+    $(document).ready(function() {
+    $('#alert').hide();
 });
 
 
